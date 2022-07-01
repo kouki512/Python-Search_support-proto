d06d18eb6aa9cab0e3e37eeb5bf6fd936fd06223
kouki512 <ht20a008@oecu.jp>
2022-07-01 17:40:20 +0900

    [add]copy suggest_word

--- search_word/views.py
+++ search_word/views.py
@@ -1,52 +1,63 @@
 from django.shortcuts import render
 # request送信後redirect先を決める reverse_lazy
 from django.urls import reverse_lazy
-from django.views.generic import CreateView,ListView,DetailView,TemplateView
+from django.views.generic import CreateView, ListView, DetailView, TemplateView
 from .models import SearchWord
+import pyperclip
+
 
 class TopPageView(TemplateView):
-  template_name = 'search_word/top.html'
+    template_name = 'search_word/top.html'
 
 
 class ListSearchWordView(ListView):
-  template_name = 'search_word/search_word_list.html'
-  model = SearchWord 
+    template_name = 'search_word/search_word_list.html'
+    model = SearchWord
+
 
 class CreateSearchWordView(CreateView):
-  template_name = 'search_word/search_word_create.html'
-  model = SearchWord 
-  fields = {'category','technique','error_message','error_detail','Feature'}
-  success_url = reverse_lazy('list_searchword')
+    template_name = 'search_word/search_word_create.html'
+    model = SearchWord
+    fields = {'category', 'technique',
+              'error_message', 'error_detail', 'Feature'}
+    success_url = reverse_lazy('list_searchword')
+
 
 class SuggestWordView(DetailView):
-  template_name = 'search_word/suggest_result.html'
-  model = SearchWord
-  
+    template_name = 'search_word/suggest_result.html'
+    model = SearchWord
+
+
 def suggest_word(words):
-  #for word in words:
-  # 提案ワードを格納する配列をオブジェクトの各要素を変数に定義
-  suggest_word = []
-  technique = words.technique 
-  error_message = words.error_message
-  error_detail = words.error_detail
-  Feature = words.Feature
-  # 提案ワードを配列に格納
-  suggest_word.append(technique + " " + error_detail)
-  suggest_word.append(technique + " " + Feature)
-  suggest_word.append(technique + " " + error_message)
-  suggest_word.append(technique + " " + error_message + " " + error_detail)
-  #print(words)
-  return suggest_word
+    # 提案ワードを格納する配列をオブジェクトの各要素を変数に定義
+    suggest_word = []
+    technique = words.technique
+    error_message = words.error_message
+    error_detail = words.error_detail
+    Feature = words.Feature
+
+    # 提案ワードを配列に格納
+    suggest_word.append(technique + " " + error_detail)
+    suggest_word.append(technique + " " + Feature)
+    suggest_word.append(technique + " " + error_message)
+    suggest_word.append(technique + " " + error_message + " " + error_detail)
+    return suggest_word
+
 
 def detail_func(request, pk):
-  object = SearchWord.objects.get(pk=pk)
-  suggest_result = suggest_word(object)
-  print(suggest_result)
-  # viewに渡す物を辞書型配列に変換
-  context = {'object': object,'suggest_result':suggest_result}
-  return render(request, 'search_word/suggest_result.html', context)
+    object = SearchWord.objects.get(pk=pk)
+    suggest_result = suggest_word(object)
+    print(suggest_result)
+    # viewに渡す物を辞書型配列に変換
+    context = {'object': object, 'suggest_result': suggest_result}
 
+    if request.method == "POST":
+        if "copy_word" in request.POST:
+          print(request.POST["copy_word"])
+          pyperclip.copy(request.POST["copy_word"])
 
+    return render(request, 'search_word/suggest_result.html', context)
 # Create your views here.
 
-
+# 他の人が解決したワードとそのサイトURLを貼ってもらう。
+# 自分の入力内容と解決した人の入力内容の近似値を求めページも出力できるようにするのがよさそう。
