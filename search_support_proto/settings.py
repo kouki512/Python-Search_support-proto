73a8c56a7ee5dcf84b58139d784ab2b7aac2aebc
kouki512 <ht20a008@oecu.jp>
2022-07-04 15:08:13 +0900

    [change]settings

--- search_support_proto/settings.py
+++ search_support_proto/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+DEBUG = False
 try:
     from .local_settings import *
 except ImportError:
     pass
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = []
 
