2563f5e1cfe4975f43544823dee0b54af81e665a
kouki512 <ht20a008@oecu.jp>
2022-07-05 17:49:19 +0900

    add

--- search_support_proto/settings.py
+++ search_support_proto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = True
+DEBUG = False
 try:
     from .local_settings import *
 except ImportError:
